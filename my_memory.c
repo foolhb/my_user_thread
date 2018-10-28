98974163b27bcbf1296c79ef030c428e2956e5a1
Bo Han <bh398@grep.cs.rutgers.edu>
2018-10-28 15:59:43 -0400

    add swap file

--- my_memory.c
+++ my_memory.c
@@ -6,6 +6,7 @@
 #include "my_pthread_t.h"
 #include <malloc.h>
 #include <math.h>
+#include <string.h>
 
 //#define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
 #define THREADREQ 1
@@ -18,17 +19,12 @@
 #define NUMBER_OF_KERNEL_PAGES 100
 #define KERNEL_MODE 1
 #define USER_MODE 0
-
+#define FILENAME "virtual_memory_file"
 typedef struct _page_table_entry {
     int page_no;
     my_pthread_t page_owner;
     int position;
 } page_table_entry;
-
-struct {
-    unsigned allocated:1;
-    unsigned size:31;
-} Header;
 
 typedef struct __node_t {
     long int head;
@@ -41,20 +37,42 @@ char *memory;
 page_table_entry page_table[NUMBER_OF_PAGES];
 
 void page_initialize(int page_no) {
-    node_t *p = &memory[page_no * PAGE_SIZE];
+    int frame_no = page_table[page_no].position;
+    if (frame_no > NUMBER_OF_FRAMES) {
+        printf("Page to initialize is not in physical memory now\n");
+        return;
+    }
+    node_t *p = &memory[frame_no * PAGE_SIZE];
     p->head = 4095 << 1;
+}
+
+
+FILE *open_virtual_memory() {
+    FILE *fp = fopen(FILENAME, "rb+");
+    return fp;
+}
+
+char *get_page_address(int page_no) {
+    char *p = &memory[page_no * PAGE_SIZE];
+    return p;
 }
 
 void initialize() {
     initialized = 1;
     printf("Memory environment Initializing! \n");
+    FILE * virtual_memory
+    if ((virtual_memory = fopen("virtual_memory", "wb")) == NULL) {
+        printf("Fail to creating virtual memory file!\n");
+    }
+    fclose(virtual_memory);
     memory = (char *) malloc(sizeof(char) * PHYSICAL_MEMORY_SIZE);
     for (int i = 0; i < NUMBER_OF_PAGES; i++) {
         page_table[i].page_no = i;
         page_table[i].page_owner = -1;
-        if (i < NUMBER_OF_FRAMES) page_table[i].position = -1;
-        else if (i < 2 * NUMBER_OF_FRAMES) page_table[i].position = 0;
-        else page_table[i].position = 1;
+        page_table[i].position = 1;
+//        if (i < NUMBER_OF_FRAMES) page_table[i].position = -1;
+//        else if (i < 2 * NUMBER_OF_FRAMES) page_table[i].position = 0;
+//        else page_table[i].position = 1;
         page_initialize(i);
     }
     printf("Memory environment complete! \n");
@@ -63,13 +81,48 @@ void initialize() {
 int find_a_free_page(int kernel_mode) {
     int i = kernel_mode ? 0 : 0 + NUMBER_OF_KERNEL_PAGES;
     for (; i < NUMBER_OF_PAGES && page_table[i].page_owner != -1; i++);
+    char *temp = (char *) malloc(sizeof(char) * PAGE_SIZE);
+
     if (i == NUMBER_OF_PAGES) {
         printf("All pages are used!\n");
         return -1;
     } else return i;
 }
 
-int swap_file(int page_to_swapin) {
+void memory_copy(char *memo1, char *memo2, int size, char *mode) {
+    char temp;
+    if (strcmp(mode, "swap") == 0) {
+        for (int i = 0; i < size; i++) {
+            temp = memo1[i];
+            memo1[i] = memo2[i];
+            memo2[i] = temp;
+        }
+    } else if (strcmp(mode, "cover") == 0) {
+        for (int i = 0; i < size; i++)
+            memo2[i] = memo1[i];
+    }
+    return;
+}
+
+int swap_file(int page_to_swapin, int page_to_evict) {
+    int temp = page_table[page_to_swapin].position;
+    page_table[page_to_swapin].position = page_table[page_to_evict].position;
+    page_table[page_to_evict].position = temp;
+    FILE *fp = open_virtual_memory();
+    int frame_no = page_to_swapin % NUMBER_OF_FRAMES;
+    char *address = get_page_address(page_to_evict);
+    int offset = (page_table[page_to_swapin].position - NUMBER_OF_FRAMES) * PAGE_SIZE;
+    char *temp_memo = (char *) malloc(sizeof(char) * PAGE_SIZE);
+    if (page_table[page_to_swapin].page_owner != -1) {
+        if (fseek(fp, offset, SEEK_SET) != 0) printf("Fseek fails \n");
+        if (fread(temp_memo, sizeof(char), PAGE_SIZE, fp) != PAGE_SIZE) printf("Copy page fails \n");
+    }
+    if (fseek(fp, offset, SEEK_SET) != 0) printf("Fseek fails \n");
+    if (fwrite(address, sizeof(char), PAGE_SIZE, fp) != PAGE_SIZE) printf("Evict page fails \n");
+    if (page_table[page_to_swapin].page_owner != -1) {
+        memory_copy(temp_memo, address, PAGE_SIZE, "cover");
+    } else page_initialize(page_to_swapin);
+    fclose(fp);
     return 0;
 }
 
